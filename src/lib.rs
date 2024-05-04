a45e2fda1405f202ba036fab21afcb48e4db8c09
Brian Vess <brian.vess@gmail.com>
2024-05-03 21:30:10 -0400

    Handle simple and hex escape codes

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,82 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn decode(input: &str) -> Result<String, String> {
+    let mut result = String::new();
+    let mut chars = input.chars().peekable();
+
+    while let Some(c) = chars.next() {
+        if c == '\\' {
+            match chars.next() {
+                // Simple excape sequences
+                Some('n') => result.push('\n'),
+                Some('t') => result.push('\t'),
+                Some('r') => result.push('\r'),
+                Some('0') => result.push('\0'),
+                Some('\\') => result.push('\\'),
+                Some('"') => result.push('"'),
+                Some('\'') => result.push('\''),
+                // 8 bit excape sequences
+                Some('x') => {
+                    let mut hex_chars = String::new();
+                    while let Some(&c) = chars.peek() {
+                        if c.is_ascii_hexdigit() {
+                            hex_chars.push(c);
+                            chars.next();
+                        } else {
+                            break;
+                        }
+                    }
+                    if let Ok(value) = u8::from_str_radix(&hex_chars, 16) {
+                        result.push(value as char);
+                    } else {
+                        return Err("Invalid String".to_string());
+                    }
+                }
+                // TODO: unicode escape sequences
+                _ => return Err("Invalid String".to_string()),
+            }
+        } else {
+            result.push(c);
+        }
+    }
+
+    Ok(result)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_simple_escape() {
+        let mut cases = Vec::new();
+        cases.push((r"\t", "\t"));
+        cases.push((r"\t\r\n", "\t\r\n"));
+        cases.push((r"\t\r\n Hello \0", "\t\r\n Hello \0"));
+
+        for case in cases {
+            assert_eq!(decode(case.0).unwrap(), case.1)
+        }
     }
+
+    #[test]
+    fn test_weight_string() {
+        let expected = "\x02 65480 LGM\r\n";
+        let case = r"\x02 65480 LGM\r\n";
+        assert_eq!(expected, decode(case).unwrap());
+    }
+
+    #[test]
+    fn test_invalid_escape() {
+        let case = r"\x02 \65480 LGM\r\n";
+        assert!(decode(case).is_err());
+        let case = r"\x02 \x65480 LGM\r\n";
+        assert!(decode(case).is_err());
+    }
+
+    // #[test]
+    // fn test_unicode_sequence() {
+    //     let expected = "↵";
+    //     let case = r"\u{21B5}";
+    //     assert_eq!(expected, decode(case).unwrap());
+    // }
 }
